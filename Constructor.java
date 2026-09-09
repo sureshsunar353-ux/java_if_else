dcd33e5754fcc463f7de361ac6cd1b33e792570e
sureshsunar353-ux <sureshsunar353@gmail.com>
2026-09-09 15:22:31 +0545

    added new file

---
+++ Constructor.java
@@ -0,0 +1,28 @@
+// create a book class with the following attributes.
+// bookid
+// title
+// author
+
+// create a parameterized constructor to initialize these values.
+// create a method displayinformation() to display the book details.
+// in main(), create two objects using the constructor and display their information.
+
+
+
+class Exercise1{
+    int bookId;
+    String title;
+    String author;
+}
+
+Exercise1(int bookId, String title, String author){
+    this.bookId=bookId;
+    this.title=title;
+    this.author=author;
+}
+
+
+
+public class Constructor {
+    
+}
---
+++ Constructor1.java
@@ -0,0 +1,17 @@
+
+// create an employee class with
+// id 
+// name
+// salary
+// create a parameterized constructor to initialize all three values. 
+// create a method displayemployee() to display the employee information.
+// create three employee objects in main()
+
+
+
+
+
+
+public class Constructor1 {
+    
+}
